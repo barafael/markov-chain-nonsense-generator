9e41193d3b6bd6db377ddc22ebc9bdde3351906b
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2021-09-02 20:57:13 +0200

    Use renamed dependency

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,6 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub fn generate_text(data: &str, rule_length: usize, result_length: usize) -> String {
-    let rule = markov_chain_text_generator::generate_rule_from_data(data, rule_length).unwrap();
-    markov_chain_text_generator::generate_text(&rule, result_length)
+    let rule = snarky_parrot::generate_rule_from_data(data, rule_length).unwrap();
+    snarky_parrot::generate_text(&rule, result_length)
 }
